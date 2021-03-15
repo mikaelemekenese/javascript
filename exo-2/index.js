cc62896a57d3149e87d08adc5a8c21ef2fde151d
Mikaele Mekenese <m.mekenese@cci-formation.nc>
2021-03-15 20:53:10 +1100

    new commit

--- exo-2/index.js
+++
@@ -1,5 +0,0 @@
-var nom = 'Doe';
-var prénom = 'John';
-var ville = 'New-York';
-
-alert('Nom: ' + nom + '\n' + 'Prénom: ' + prénom + '\n' + 'Ville: ' + ville);
--- exo-3/index.js
+++
@@ -1,5 +0,0 @@
-var nom = 'Doe';
-var prénom = 'John';
-var ville = 'New-York';
-
-prompt("Bonjour, ", prénom);
