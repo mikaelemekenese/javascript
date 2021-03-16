59cbdb7851c43202623f4e2b51cf01b8791dfccb
Mikaele Mekenese <m.mekenese@cci-formation.nc>
2021-03-17 07:53:52 +1100

    new commit

---
+++ todo-list/app.js
@@ -0,0 +1,35 @@
+window.onload = () => {
+    const form1 = document.querySelector('#addForm1');
+    const form2 = document.querySelector('#addForm2');
+  
+    let items = document.getElementById('items');
+    let submit = document.getElementById('submit');
+  
+    let editItem = null;
+  
+    form1.addEventListener('submit', addItem);
+    items.addEventListener('click', removeItem);
+};
+  
+function addItem(e) {
+    e.preventDefault();
+    /**
+     *
+     * a vous de jouer
+     */
+};
+
+function removeItem(e) {
+    e.preventDefault();
+    /**
+     *
+     * a vous de jouer
+     */
+};
+
+function toggleButton(ref, btnID) {
+    /**
+     *
+     * a vous de jouer
+     */
+};
