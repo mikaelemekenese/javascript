7038d0704a0e1cd2a54c02753ded2fef3823d0f7
Mikaele Mekenese <m.mekenese@cci-formation.nc>
2021-03-27 22:39:24 +1100

    new commit

---
+++ la-manu-pt4/questions.js
@@ -0,0 +1,65 @@
+/**
+ * Exercice sur les chaines de caractères.
+ * Trouvez la façon de faire la plus optimale.
+ * Il peut y avoir plusieurs façons de faire. 
+ */
+
+var tailleString = function (texte) {
+    return 30;
+}
+var remplaceECar = function (texte) {
+    return 'C  texte à une certaine taille';
+}
+var concatString = function (texte1, texte2) {
+    return 'Le javascript est super';
+}
+var afficherCar5 = function (texte) {
+    return 'e';
+}
+var afficher9Car = function (texte) {
+    return 'Je dirait';
+}
+var majusculeString = function (texte) {
+    return 'UN AUTRE EXCELLENT EXERCICE';
+}
+var minusculeString = function (texte) {
+    return 'celui ci aussi!';
+}
+var SupprEspaceString = function (texte) {
+    return "Rome ne s'est pas construite en un jour";
+}
+var IsString = function (texte) {
+    return true;
+}
+var AfficherExtensionString = function (texte) {
+    return 'jpg';
+}
+var NombreEspaceString = function (texte) {
+    return 5;
+}
+var InverseString = function (texte) {
+    return "?iuh'druojua'd ritrap à ,niamed sèrpA";
+}
+
+/**
+ * Exercices sur les nombres et les calculs mathématiques
+ */
+
+var calculPuissance = function (x, y) {
+    return 8;
+}
+var valeurAbsolue = function (nombre) {
+    return 5;
+}
+var valeurAbsolueArray = function (array) {
+    return [ 5, 50, 25, 568 ];
+}
+var sufaceCercle = function (rayon) {
+    return 79;
+}
+var hypothenuse = function (ab, ac) {
+    return 9.433981132056603;
+}
+var calculIMC = function (poids, taille) {
+    return 21.22;
+}
---
+++ la-manu-pt5/questions.js
@@ -0,0 +1,43 @@
+var languagesArrayCreation = function () {
+  return ["Html","CSS","Java","PHP"];
+}
+
+var numbersArrayCreation = function () {
+  return [ 0, 1, 2, 3, 4, 5 ];
+}
+
+var ElementReplacement = function (languages) {
+  return [ 'Html', 'CSS', 'Javascript', 'PHP' ];
+}
+
+var AddElementToLanguagesArray = function (languages) {
+  return [ 'Html', 'CSS', 'Javascript', 'PHP', 'Ruby', 'Python' ];
+}
+
+var AddElementToNumbersArray = function (numbers) {
+  return [ -2, -1, 0, 1, 2, 3, 4, 5 ];
+}
+
+var deleteArrayFirstElement = function (languages) {
+  return [ 'CSS', 'Javascript', 'PHP', 'Ruby', 'Python' ];
+}
+
+var deleteArrayLastElement = function (languages) {
+  return [ 'CSS', 'Javascript', 'PHP', 'Ruby' ];
+}
+
+var stringToArray = function (socialMediaInString) {
+  return [ 'Facebook', 'Twitter', 'Google +', 'Viadeo', 'LinkedIn' ];
+}
+
+var arrayToString = function (languages) {
+  return 'CSS,Javascript,PHP,Ruby';
+}
+
+var arraySort = function (socialMedia) {
+  return [ 'Facebook', 'Google +', 'LinkedIn', 'Twitter', 'Viadeo' ];
+}
+
+var arrayInvert = function (languages){
+  return [ 'Ruby', 'PHP', 'Javascript', 'CSS' ];
+}
