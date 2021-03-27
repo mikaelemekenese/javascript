fcd05bcf83f7568c25f2e38aa0275dc28cd8f654
Mikaele Mekenese <m.mekenese@cci-formation.nc>
2021-03-27 23:18:19 +1100

    new commit

--- todo-list/app.js
+++ todo-list/app.js
@@ -1,22 +1,30 @@
+const form1 = document.querySelector('#addForm1');
+const form2 = document.querySelector('#addForm2');
+
+let item = document.getElementById('item');
+let items = document.getElementById('items');
+let submit = document.getElementById('submit');
+let text = document.getElementById('lblsuccess');
+
+let editItem = null;
+
 window.onload = () => {
-    const form1 = document.querySelector('#addForm1');
-    const form2 = document.querySelector('#addForm2');
-  
-    let items = document.getElementById('items');
-    let submit = document.getElementById('submit');
-  
-    let editItem = null;
-  
-    form1.addEventListener('submit', addItem);
+    form1.addEventListener('click', addItem);
     items.addEventListener('click', removeItem);
 };
   
 function addItem(e) {
     e.preventDefault();
-    /**
-     *
-     * a vous de jouer
-     */
+    
+    if (item.value == '') {
+        text.style.display = 'initial';
+        text.innerHTML = "Veuillez entrer une tâche !";
+        submit.disabled = true;
+    } else {
+        text.style.display = 'initial';
+        text.innerHTML = "Tâche enregistrée !";
+        submit.disabled = false;
+    };
 };
 
 function removeItem(e) {
