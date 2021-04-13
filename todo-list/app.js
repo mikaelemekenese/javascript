c821f8f38be5114e9c3c035e7e7683e1e448742a
mikaelemekenese <m.mekenese@cci-formation.nc>
2021-04-13 12:04:36 +1100

    new commit

--- todo-list/app.js
+++ todo-list/app.js
@@ -1,43 +1,90 @@
-const form1 = document.querySelector('#addForm1');
-const form2 = document.querySelector('#addForm2');
-
-let item = document.getElementById('item');
-let items = document.getElementById('items');
-let submit = document.getElementById('submit');
-let text = document.getElementById('lblsuccess');
-
-let editItem = null;
-
 window.onload = () => {
-    form1.addEventListener('click', addItem);
-    items.addEventListener('click', removeItem);
-};
+    const form = document.querySelector('#addForm');
   
-function addItem(e) {
+    let items = document.getElementById('items');
+    let submit = document.getElementById('submit');
+  
+    let editItem = null;
+  
+    form.addEventListener('submit', addItem);
+    items.addEventListener('click', removeItem);
+  };
+  
+  function addItem(e) {
     e.preventDefault();
-    
-    if (item.value == '') {
-        text.style.display = 'initial';
-        text.innerHTML = "Veuillez entrer une tâche !";
-        submit.disabled = true;
+    console.log('addItem');
+  
+    if (submit.value != 'Submit') {
+      console.log('! Submit');
+  
+      editItem.target.parentNode.childNodes[0].data = document.getElementById('item').value;
+  
+      submit.value = 'Submit';
+      document.getElementById('item').value = '';
+  
+      document.getElementById('lblsuccess').innerHTML = 'Text edited successfully';
+  
+      document.getElementById('lblsuccess').style.display = 'block';
+  
+      setTimeout(function () {
+        document.getElementById('lblsuccess').style.display = 'none';
+      }, 3000);
+  
+      return false;
+    }
+  
+    let newItem = document.getElementById('item').value;
+    if (newItem.trim() === '' || newItem.trim() === null) {
+      return false;
     } else {
-        text.style.display = 'initial';
-        text.innerHTML = "Tâche enregistrée !";
-        submit.disabled = false;
-    };
-};
-
-function removeItem(e) {
+      document.getElementById('item').value = '';
+    }
+  
+    let li = document.createElement('li');
+    li.className = 'list-group-item';
+  
+    let deleteButton = document.createElement('button');
+  
+    deleteButton.className = 'btn-danger btn btn-sm float-right delete';
+  
+    deleteButton.appendChild(document.createTextNode('Delete'));
+  
+    let editButton = document.createElement('button');
+  
+    editButton.className = 'btn-success btn btn-sm float-right edit';
+  
+    editButton.appendChild(document.createTextNode('Edit text'));
+  
+    li.appendChild(document.createTextNode(newItem));
+    li.appendChild(deleteButton);
+    li.appendChild(editButton);
+  
+    items.appendChild(li);
+  }
+  
+  function removeItem(e) {
     e.preventDefault();
-    /**
-     *
-     * a vous de jouer
-     */
-};
-
-function toggleButton(ref, btnID) {
-    /**
-     *
-     * a vous de jouer
-     */
-};
+    console.log('removeItem');
+    if (e.target.classList.contains('delete')) {
+      if (confirm('Are you Sure?')) {
+        let li = e.target.parentNode;
+        items.removeChild(li);
+        document.getElementById('lblsuccess').innerHTML = 'Text deleted successfully';
+  
+        document.getElementById('lblsuccess').style.display = 'block';
+  
+        setTimeout(function () {
+          document.getElementById('lblsuccess').style.display = 'none';
+        }, 3000);
+      }
+    }
+    if (e.target.classList.contains('edit')) {
+      document.getElementById('item').value = e.target.parentNode.childNodes[0].data;
+      submit.value = 'EDIT';
+      editItem = e;
+    }
+  }
+  
+  function toggleButton(ref, btnID) {
+    document.getElementById(btnID).disabled = false;
+  }
